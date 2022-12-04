2d7f41397b63c4f1208ea23a6b0167ca8a3b105e
Sagar Jaid <sagarjaid321@gmail.com>
2022-12-04 23:51:14 +0530

    contact us page added

--- pages/location.tsx
+++ pages/location.tsx
@@ -6,27 +6,10 @@ import Header from '../components/main/Header'
 const Contact = () => {
     return (
         <>
-            <section className="bg-white dark:bg-gray-900">
-                <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
-                    <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Feel Free to get it Touch.</p>
-                    <form action="#" className="space-y-8">
-                        <div>
-                            <input type="email" id="email" className="shadow-sm  border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@gmail.com" required />
-                        </div>
-                        <div>
-                            <input type="number" id="number" className="shadow-sm  border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="+91 XXXXXXXXXX" required />
-                        </div>
-                        <div>
-                            <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900  rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let us know how we can help you" required />
-                        </div>
-                        <div className="sm:col-span-2">
-                            <textarea id="message" className="block p-2.5 w-full text-sm text-gray-900  rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..." />
-                        </div>
-                        <button type="submit" className="py-3 px-5 text-sm font-medium text-center bg-blue-500 text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
-                    </form>
-                </div>
-            </section>
+            <div className='flex flex-col gap-2 justify-center items-center px-4 py-24 h-[60vh]'>
+                <p className="text-center sm:text-xl">Feel Free to get it Touch.</p>
+                <a className="text-blue-500 text-center" href="mailto:careerdekhoai@gmail.com">careerdekhoai@gmail.com</a>
+            </div>
         </>
     )
 }
